bab4304d3a7cf078af51121be16a682cfe32c2d8
shumaher205 <prosto-misha2008@mail.ru>
2020-09-28 12:16:55 +0300

    IT-6524: Added endpoints with dummy responses

---
+++ ThirdPartyIntegrationSample/Web/Controllers/IntegrationInfoController.cs
@@ -0,0 +1,18 @@
+﻿using Billwerk.Payment.SDK.DTO.IntegrationInfo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class IntegrationInfoController : ApiControllerBase
+    {
+        [HttpGet]
+        [Route("api/integrationInfo/{id}")]
+        public ExternalIntegrationInfoDTO Get(string id)
+        {
+            return new ExternalIntegrationInfoDTO();
+        }
+    }
+}
---
+++ ThirdPartyIntegrationSample/Web/Controllers/PaymentsCancellationController.cs
@@ -0,0 +1,18 @@
+﻿using Billwerk.Payment.SDK.DTO.Cancellation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class PaymentsCancellationController : ApiControllerBase
+    {
+        [HttpPost]
+        [Route("api/payment/{id}/cancel")]
+        public ExternalPaymentCancellationDTO Cancel(string id)
+        {
+            return new ExternalPaymentCancellationDTO();
+        }
+    }
+}
