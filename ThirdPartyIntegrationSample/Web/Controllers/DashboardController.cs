45543cf5719de1cf797ef9d0a6ffbbeb5e28486b
Mikhail Petrushevskiy <mikhail.petrushevskiy@billwerk.com>
2020-10-02 16:16:18 +0300

    No-JIRA: Experiments

--- ThirdPartyIntegrationSample/Web/Controllers/DashboardController.cs
+++ ThirdPartyIntegrationSample/Web/Controllers/DashboardController.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
-    [Authorize]
     public class DashboardController : ControllerBase
     {
-        [HttpGet]
-        [AllowAnonymous]
         public RedirectResult Get()
         {
             return Redirect("/hangfire");
