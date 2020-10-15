6f4f2b4a875e81ac0d2e258ba1776c6c8f7058b9
Alexey Sokolov <alexey.sakalou@billwerk.com>
2020-10-15 17:17:53 +0300

    IT-6571: 3rd party: Extend refund endpoint

--- ThirdPartyIntegrationSample/Business/Interfaces/IPaymentService.cs
+++ ThirdPartyIntegrationSample/Business/Interfaces/IPaymentService.cs
@@ -1,16 +1,17 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Billwerk.Payment.SDK.DTO.ExternalIntegration.Cancellation;
 using Billwerk.Payment.SDK.DTO.ExternalIntegration.Payment;
 using Billwerk.Payment.SDK.DTO.ExternalIntegration.Preauth;
 using Billwerk.Payment.SDK.DTO.ExternalIntegration.Refund;
 using Business.Models;
+using Persistence.Models;
 
 namespace Business.Interfaces
 {
     public interface IPaymentService
     {
         Task<ExternalPaymentResponse> SendPayment(ExternalPaymentRequest paymentRequest);
-        Task<ExternalRefundTransactionDTO> SendRefund(ExternalRefundRequestDTO dto);
+        Task<ExternalRefundTransactionDTO> SendRefund(ExternalRefundRequestDTO dto, PaymentTransactionBase targetTransaction);
         Task<ExternalPreauthTransactionDTO> SendPreauth(ExternalPreauthRequestDTO dto);
         Task<ExternalPaymentCancellationDTO> SendCancellation(ExternalPreauthRequestDTO dto);
         Task<ExternalPaymentTransactionDTO> FetchPayment(string transactionId);
