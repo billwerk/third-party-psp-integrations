3d48944044e7d6ccad5224bff980d2a9e622794d
Przemek Dawidowicz <przemyslaw.dawidowicz@billwerk.com>
2022-11-28 06:45:44 +0100

    Description change made by PD.

--- PaymentGateway/PaymentGateway/AppBuilder.cs
+++ PaymentGateway/PaymentGateway/AppBuilder.cs
@@ -76,7 +76,8 @@ public static class AppBuilder
                 Title = "Payment Gateway",
                 Description = "Following REST API documentation describes all data components that are included in the implementation of the 3rd party PSP integration. It enlists endpoints playing a major role in the process of pre-authorization, payments and refunds. " +
                               @"The additional Schema section consists of Data transfer objects (DTOs) of requests and responds with descriptions of their parameters.  
-                                To have better understanding of the Payment Gateway integration, please check our public documentation [Payment Gateway](https://payment-gateaway-billwerk.gitbook.io/home/).",
+                                To have better understanding of the Payment Gateway integration, please check our public documentation [Payment Gateway](https://billwerk.gitbook.io/home/). or our [repository](https://github.com/billwerk/third-party-psp-integrations) with a sample application.  
+                                [Contact the support](support@billwerk.com).",
                 Version = "v1",
             });
         });
